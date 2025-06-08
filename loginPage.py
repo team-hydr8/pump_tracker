0718c52d186b2a4134b8f5b19a83ec832f1bdb79
Eldridge <hvm23zmu@uea.ac.uk>
2025-06-08 15:45:30 +0100

    Add files via upload

--- loginPage.py
+++ loginPage.py
@@ -16,29 +16,83 @@ class Login(ttk.Frame):
         super().__init__(parent)
         self.controller = controller
         
+        self.columnconfigure(0, weight=1)
+        #self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+
+        #Opening and creating user icon image
+        self.userIcon = Image.open("images/userIcon.png").resize((100, 100))
+        self.userIcon = ImageTk.PhotoImage(self.userIcon)
+
+        #Using label to place the user icon image
+        self.userIcon_label = tk.Label(self, image=self.userIcon)
+        self.userIcon_label.grid(row=0, column=0, columnspan=2, sticky="new", pady=30)
+
         notification = LoginBox(self,controller=controller)
-        notification.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
+        notification.grid(row=1, column=0, sticky="new", padx=5)
 
 class LoginBox(ttk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
 
-        def submit():          
+        def submit():
+            #If login is successful
             if current_backend.login(nameBox.get(),passwordBox.get()):
                 controller.show_frame(homePage.Home)
-            
+            #If login is unsuccessful
+            else:
+                loginWarning = ttk.Label(self, text = '*Incorrect user ID or password', font=('calibre',10, 'bold'))
+                loginWarning.grid(row=3, column=0, columnspan=2, sticky="ew", pady=2)
+
+        #Allows placeholder text inside the username entry box    
+        def nameBox_focus_in(event):
+            if nameBox.get() == "Username":
+                nameBox.delete(0, tk.END)
+                nameBox.configure(foreground="black")
+
+        #Placeholder text can reappear if username entry box is empty
+        def nameBox_focus_out(event):
+            if nameBox.get() == "":
+                nameBox.insert(0, "Username")
+                nameBox.configure(foreground="gray")
+
+        #Allows placeholder text inside the password entry box    
+        def passwordBox_focus_in(event):
+            if passwordBox.get() == "Password":
+                passwordBox.config(show="*")
+                passwordBox.delete(0, tk.END)
+                passwordBox.configure(foreground="black")
+
+        #Placeholder text can reappear if password entry box is empty
+        def passwordBox_focus_out(event):
+            if passwordBox.get() == "":
+                passwordBox.config(show="")
+                passwordBox.insert(0, "Password")
+                passwordBox.configure(foreground="gray")
 
         self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=1)
         self.rowconfigure(3, weight=1)
 
-        nameLabel = tk.Label(self, text = 'Username', font=('calibre',10, 'bold'))
-        nameBox = tk.Entry(self, font=('calibre',10,'normal'))
-        passwordLabel = tk.Label(self, text = 'Password', font = ('calibre',10,'bold'))
-        passwordBox = tk.Entry(self, font = ('calibre',10,'normal'), show = '*')
-        submitButton=tk.Button(self,text = 'Submit', command = submit)
-        nameLabel.grid(row=0,column=0)
-        nameBox.grid(row=0,column=1)
-        passwordLabel.grid(row=1,column=0)
-        passwordBox.grid(row=1,column=1)
-        submitButton.grid(row=2,column=1)
+        #Username entry details
+        #nameLabel = ttk.Label(self, text = 'Username', font=('calibre',10, 'bold'))
+        nameBox = ttk.Entry(self, font=('calibre',10,'normal'), foreground="gray")
+        nameBox.insert(0, "Username")
+        nameBox.bind("<FocusIn>", nameBox_focus_in)
+        nameBox.bind("<FocusOut>", nameBox_focus_out)
+
+        #Password entry details
+        #passwordLabel = ttk.Label(self, text = 'Password', font = ('calibre',10,'bold'))
+        passwordBox = ttk.Entry(self, font = ('calibre',10,'normal'), foreground="gray")
+        passwordBox.insert(0, "Password")
+        passwordBox.bind("<FocusIn>", passwordBox_focus_in)
+        passwordBox.bind("<FocusOut>", passwordBox_focus_out)
+
+        submitButton=ttk.Button(self,text = 'Submit', command = submit)
+        #nameLabel.grid(row=1,column=0)
+        nameBox.grid(row=0,column=0, columnspan=2, sticky="ew", pady=5)
+        #passwordLabel.grid(row=2,column=0)
+        passwordBox.grid(row=1,column=0, columnspan=2, sticky="ew", pady=5)
+        submitButton.grid(row=2,column=0, columnspan=2, pady=10)

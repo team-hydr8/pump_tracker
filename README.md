3f3c84f4981e09e61940e8b11b70a3b67a7d46fe
Zekiah-A <zekiahamoako@outlook.com>
2025-06-10 15:23:37 +0100

    Fix iconbitmap error, add user documentation and cleanup unused files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # pump_tracker
+
+## Running the program:
+```sh
+pip install -r requirements.txt
+python main.py
+```
+
+### If using venv:
+```sh
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+python main.py
+```
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from tkinter import PhotoImage
 from appBase import Application
 
 app = Application()
-app.iconbitmap(r'images/logo.ico')
+try:
+    app.iconbitmap("images/logo.ico")
+except:
+    # On MacOS, Linux and other platforms iconbitmap throws an error so use this instead
+    # https://stackoverflow.com/questions/52826692/set-tkinter-icon-on-mac-os
+    img = PhotoImage(file="images/logo.png")
+    app.tk.call("wm", "iconphoto", app._w, img)
+
 app.mainloop()
